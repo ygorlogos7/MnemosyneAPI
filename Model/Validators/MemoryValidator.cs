1de4106773745448c44daaa27952d313d7c87d9e
fic <fic@NOTE02-S28>
2025-02-27 21:51:39 -0300

    Fazendo integração com front

--- Model/Validators/MemoryValidator.cs
+++ Model/Validators/MemoryValidator.cs
@@ -17,6 +17,7 @@ namespace MnemosyneAPI.Model.Validates
             RuleFor(x => x.images)
                 .NotNull().WithMessage("Imagem e obrigatoria")
                 .NotEmpty().WithMessage("Imagem nao pode ser vazio");
+
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,25 +8,43 @@ using ProjetoAPI.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHealthChecks();
+
+var MinhasOrigens = "_minhasorigens";
+
+builder.Services.AddCors(
+    options => {
+        options.AddPolicy(
+        name: MinhasOrigens,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173");
+            policy.AllowAnyHeader();
+            policy.AllowAnyMethod();
+        });
+    }    
+);
+
 builder.Services.AddValidatorsFromAssemblyContaining<MemoryValidator>();
 
 // Adiciona o serviço do Swagger
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo
     {
-        Title = "Mnemosyne API",
+        Title = " API Mnemosyne ",
         Version = "v1",
         Description = "API  desenvolvida no curso de Programaçao em C#, para atender ao Frotend do site Mnemosyne"
-        
     });
 });
 
 // Adiciona o serviço do banco de dados SQLite
 builder.Services.AddDbContext<MemoryDbContext>
-(options =>options.UseSqlite("Data Source=memories.db"));
 
+(options =>options.UseSqlite("Data Source=memories.db"));
+    
 var app = builder.Build();
 
 // Ativa o Swagger na aplicação
@@ -40,6 +58,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapMemoryEndpoints();
+
+app.UseCors(MinhasOrigens);
 
 // Já vem na aplicação!!
 app.Run();
